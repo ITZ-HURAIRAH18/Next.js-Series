6f67345c2cd845d3c9b020f1484302b36d78c590
ITZ-HURAIRAH18 <muhammadabuhurairah558@gmail.com>
2025-11-05 22:15:24 +0500

    Lect 5

--- 05) Route Group/next.js/app/(users)/layout.js
+++ 05) Route Group/next.js/app/(users)/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 
+import Navbar from "../../component/Navbar";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -22,6 +22,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Navbar/>
+        <h1>THis is From main layout</h1>
+
         {children}
       </body>
     </html>
