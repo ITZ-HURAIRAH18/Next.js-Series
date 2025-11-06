52c5c3ee19df2dc876ef26280b00d91f30f9516f
ITZ-HURAIRAH18 <muhammadabuhurairah558@gmail.com>
2025-11-06 15:03:51 +0500

    Lect 6

---
+++ 06) Modules Css/my-app/app/(users)/contact/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import style from "./contact.module.css"
+function Page() {
+  return (
+    <h1 className={style.title}>This is Contact us page</h1>
+  )
+}
+
+export default Page
