66bb0a9ffa0c54f86e1b3984cca3b9a3c6850a73
ITZ-HURAIRAH18 <muhammadabuhurairah558@gmail.com>
2025-11-09 13:32:41 +0500

    Connect Supabase with Next.js '

---
+++ 20)   Connect Next.js with MySQL/my-app/app/(users)/fetchdata/clientside/page.jsx
@@ -0,0 +1,38 @@
+'use client';
+
+import React, { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+import Loading from "@/app/(users)/fetchdata/loading";
+// http://localhost:3000/fetchdata/clientside?name=ali
+const Page = () => {
+  const searchParams = useSearchParams();
+  const userName = searchParams.get("name") || "Aftab";
+
+  const [genderData, setGenderData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(`https://api.genderize.io?name=${userName}`);
+      const data = await res.json();
+      setGenderData(data);
+    };
+    fetchData();
+  }, [userName]);
+
+  if (!genderData) return <Loading/>;
+
+  return (
+    <div className="p-6">
+      <h1 className="text-xl font-bold mb-4">Gender Prediction</h1>
+
+      <div className="space-y-2 border p-4 rounded-lg shadow-md">
+        <p><strong>Name:</strong> {genderData.name}</p>
+        <p><strong>Gender:</strong> {genderData.gender}</p>
+        <p><strong>Probability:</strong> {genderData.probability}</p>
+        <p><strong>Count:</strong> {genderData.count}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
